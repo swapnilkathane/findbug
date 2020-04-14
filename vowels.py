d0712b88101a1009b0334532392c477a2f89cc04
swapnilkathane <62869345+swapnilkathane@users.noreply.github.com>
2020-04-15 00:01:24 +0530

    Initial commit

---
+++ vowels.py
@@ -0,0 +1,23 @@
+def is_vowel(letter):
+    return letter in ['a', 'e', 'i', 'o', 'u']
+
+def score_words(words):
+    score = 0
+    for word in words:
+        num_vowels = 0
+        for letter in word:
+            if is_vowel(letter):
+                num_vowels += 1
+                #print(num_vowels)
+        if num_vowels % 2 == 0:
+            score += 2
+            print(score)
+        else:
+            score+=1
+            print(score)
+    return score
+
+
+n = int(input())
+words = input().split()
+print(score_words(words))
